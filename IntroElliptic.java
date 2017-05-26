a02ede280ce46bf62a6632d1d884bd9c976d8ba2
Haoyu Zhang <haoyu.zhang@mail.mcgill.ca>
2017-05-26 14:14:27 -0400

    Update IntroElliptic.java

--- IntroElliptic.java
+++ IntroElliptic.java
@@ -13,5 +13,30 @@ public class IntroElliptic
 				return true;
 			return false;
 	}
+	public static long[][] generateNRationalNumbers(long N)
+	{
+		long[][] rationals=new long[(int)N][2];
+		long[] DiatomicSerieUntilN=generateSternDiatomicSerie(N);
+		for( long i=1;i<=N;i++)
+		{
+			rationals[i][0]=DiatomicSerieUntilN[];
+			rationals[i][1]=DiatomicSerieUntilN[];
+		}
+		return rationals;
+	}
+	public static long[] generateSternDiatomicSerie(long n)
+	{
+		long[] dioatimicSerieTable= new long[(int)n+1];
+		dioatimicSerieTable[0]=0;
+		dioatimicSerieTable[1]=1;
+		for(long i=2;i<=n;i++)
+			if(i%2==0)
+				dioatimicSerieTable[(int)i]=dioatimicSerieTable[(int)i/2];
+			else
+				dioatimicSerieTable[(int)i]=dioatimicSerieTable[((int)i-1)/2]+dioatimicSerieTable[((int)i-1)/2-1];
+				
+		return dioatimicSerieTable;
+		
+	}
 
 }
