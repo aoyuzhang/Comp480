046bbca163e69d1c162cfb39fc428d468e4a7907
Haoyu Zhang <haoyu.zhang@mail.mcgill.ca>
2017-05-28 14:18:49 -0400

    Update IntroElliptic.java

--- IntroElliptic.java
+++ IntroElliptic.java
@@ -4,7 +4,9 @@ public class IntroElliptic
 
 	public static void main(String[] args) 
 	{
-		
+		long[][] rationals=generateNRationalNumbers(100);
+		for(int i=0;i<100;i++)
+			printRational(rationals[i][0],rationals[i][1]);
 
 	}
 	public static boolean isPythagorianTriple(int a, int b, int c)
@@ -15,12 +17,12 @@ public class IntroElliptic
 	}
 	public static long[][] generateNRationalNumbers(long N)
 	{
-		long[][] rationals=new long[(int)N][2];
-		long[] DiatomicSerieUntilN=generateSternDiatomicSerie(N);
+		long[][] rationals=new long[(int)N+1][2];
+		long[] DiatomicSerieUntilN=generateSternDiatomicSerie(N+1);
 		for( long i=1;i<=N;i++)
 		{
-			rationals[i][0]=DiatomicSerieUntilN[];
-			rationals[i][1]=DiatomicSerieUntilN[];
+			rationals[(int)i][0]=DiatomicSerieUntilN[(int)i];
+			rationals[(int)i][1]=DiatomicSerieUntilN[(int)i+1];
 		}
 		return rationals;
 	}
@@ -33,10 +35,15 @@ public class IntroElliptic
 			if(i%2==0)
 				dioatimicSerieTable[(int)i]=dioatimicSerieTable[(int)i/2];
 			else
-				dioatimicSerieTable[(int)i]=dioatimicSerieTable[((int)i-1)/2]+dioatimicSerieTable[((int)i-1)/2-1];
+				dioatimicSerieTable[(int)i]=dioatimicSerieTable[((int)i-1)/2]+dioatimicSerieTable[((int)i-1)/2+1];
 				
+		for(int i=1;i<=n;i++)
+			System.out.println(dioatimicSerieTable[i]);
 		return dioatimicSerieTable;
-		
+	}
+	public static void printRational(long a, long b)
+	{
+		System.out.println(a+"/"+b);
 	}
 
 }
